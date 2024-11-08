25599435a55d3c3d4f3f33fa20236a4d45c29974
nerr-0 <nduguiskarioti@gmail.com>
2024-11-08 18:25:20 +0300

    UI design

--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 
 const db = require('./db');
-const bcrypt = require('bcryptjs');
-
-
+// const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt")
 //my code 
 //creating connection to database
 
@@ -13,6 +12,7 @@ const bcrypt = require('bcryptjs');
 
         return result;
     }
+// The code below is used to search for users in the quoted databases and tables by vaious parameters
 function findByUsername(username){
     const [rows] = con.query("SELECT * FROM users WHERE username = ?", [username]);
     return rows[0]
@@ -21,31 +21,12 @@ function findById(id){
     const[rows] = con.query("SELECT * FROM users WHERE id = ?", [id]);
     return rows[0]
 }
+//The code below is used to validate user credentials at sign in, sign up and any other place deemed necessary
 function validatePassword(enteredPassword, storedPassword){
     return bcrypt.compare(enteredPassword, storedPassword)
 }
 //  export {createUser, findByUsername, findById};
- module.exports = { createUser, findByUsername, findById }
+ module.exports = { createUser, findByUsername, findById, validatePassword }
 
 
 
-// class User{
-//     function createUser(username, password){
-//         const hashedPassword = bcrypt.hash(password,10)
-//         const [result] = con.query("INSERT INTO users (username, password) VALUES(?,?", [username,hashedPassword]);
-
-//         return result;
-//     }
-// function findByUsername(username){
-//     const [rows] = con.query("SELECT * FROM users WHERE username = ?", [username]);
-//     return rows[0]
-// }
-// function findById(id){
-//     const[rows] = con.query("SELECT * FROM users WHERE id = ?", [id]);
-//     return rows[0]
-// }
-// function validatePassword(enteredPassword, storedPassword){
-//     return bcrypt.compare(enteredPassword, storedPassword)
-// }
-// }
-// module.exports = User;
