0ae8cfb748b657448b1b56da3f9bf4b965816fd6
nerr-0 <nduguiskarioti@gmail.com>
2024-11-14 19:23:18 +0300

    database update

--- models/db.js
+++ models/db.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql')
+// const mysql = require('mysql')
 
 
-const con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-  });
-  con.connect((error) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log("CONNECTED TO  DATABASE");
-    }
-})
-module.exports = con;
+// const con = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+//   });
+//   con.connect((error) => {
+//     if (error) {
+//       console.error(error);
+//     } else {
+//       console.log("CONNECTED TO  DATABASE");
+//     }
+// })
+// module.exports = con;
